6f800e8a86294514aa8121222fdc5bb91da0f8c8
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-09-24 15:31:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 function setup() {
     canvas=createCanvas(400,400)
     canvas.position(500,150)
+
+    video=createCapture(VIDEO)
+    video.size(400,400)
+    PoseNet=ml5.poseNet(video,model_loaded)
+    PoseNet.on("pose",getPoses)
+}
+function model_loaded() {
+    console.log("Model is loaded")
 }
 function draw() {
-    
+    background("#13d6a2")
+}
+function getPoses(results) {
+    if (results.length>0) {
+        console.log(results)
+    }
 }
